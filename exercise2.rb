7e8c26503288cfdabbfa22de21ac3dfd7d3311e6
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-27 14:23:24 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+#1 - Calculate tip for meal
+
+puts 55 * 1.15
+
+#2 - add string and integer
+
+puts 2.to_s + " Meggies are better than " + 1.to_s
+
+#3 - string interpolation
+
+puts "45628 multiplied by 7839 equals #{45628 * 7839}"
+
+#4 - true or false
+
+puts (10 < 20 && 30 < 20) || !(10 == 11)
