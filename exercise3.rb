607ac233d51897f3ededce9f208567986e4613e7
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-27 14:46:30 -0400

    ask the user for their age and calculate year they were born, depending on if their birthday has passed or not

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,12 @@
 puts "What is your name?"
+
+name = gets.chomp
+
+puts "Hi #{name}!"
+
+puts "#{name}, how old are you?"
+
+age = gets.to_i
+
+
+puts "#{name}, you were born in the year #{2017 - age} if your birthday has already passed and the year #{2016 -age} if you birthday has not yet passed."
