dec0e0de1270debe5390888ec2d349e7a18e1a2d
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-27 16:02:36 -0400

    add actions - rest and eat to program

--- exercise6.rb
+++ exercise6.rb
@@ -4,21 +4,35 @@
 
 action = ""
 distance = 0
+energy = 10
 
 while action != "home"
-  puts "What would you like to do now (walk, run, home)?"
+  puts "What would you like to do now?"
   action = gets.chomp
 
   if action == "walk"
     puts "Distance from home is #{distance += 1} km"
+    energy += 1
 
-  elsif action == "run"
+  elsif action == "run" && energy > 0
     puts "Distance from home is #{distance += 5} km"
+    energy -= 5
 
   elsif action == "home"
     puts "Exercise session over, you have travelled #{distance} kms"
 
+  elsif energy <= 0
+    puts "You are too tired to run, please either walk, rest, eat or go home"
+
+  elsif action == "eat"
+    puts "delicious!"
+    energy += 5
+
+  elsif action == "rest"
+    puts "you must be tired!"
+    energy += 2
+
   else
-    puts "Command not found, please use: run, walk, home"
+    puts "Command not found, please use: run, walk, home, eat, rest"
 end
 end
