b526bfc5b620d73c0fd5853e55d7ef5e58e87478
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-28 09:52:57 -0400

    write pseudocode for BitMaker assignment

---
+++ pseudocode.rb
@@ -0,0 +1,6 @@
+#Store a list of numbers from 1 to 100
+#For each number in the list mod 3 and mod 5
+  #If mod 3 and mod 5 both equal 0, print "Bitmaker" instead of the number
+  #If mod 3 equals 0, print "Bit" instead of the number
+  #If mod 5 equals 0, print "Maker" instead of the number
+  #For all other numbers, print the number
